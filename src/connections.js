6765ea514834fa9ee231f503a60e68add3af6d12
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2022-07-21 08:53:19 -0400

    Las variables de entorno han sido configuradas

--- src/connections.js
+++ src/connections.js
@@ -1,11 +1,14 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'admin',
-    database: 'appContactos',
-    port: 5432
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.PORT_DB,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 module.exports = {pool}
